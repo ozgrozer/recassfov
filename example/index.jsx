68a1f0057a289487c12d273fccd647cb36239e92
Ozgur <ozgr@live.com>
2018-05-03 15:57:11 +0300

    Remove unnecassary variable.

--- example/index.jsx
+++ example/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Form, Input, InvalidFeedback } from './../src/Recassfov'
+import { Form, Input } from './../src/Recassfov'
 
 const validations = {
   username: [
